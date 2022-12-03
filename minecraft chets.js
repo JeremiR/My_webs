bfdc3ea47523c7dacf33fd3e99f4bbfe50b5e335
JeremiR <jeremi.r2012@gmail.com>
2022-12-03 16:33:20 +0100

    make
    a js file

---
+++ minecraft chets.js
@@ -0,0 +1,25 @@
+console.log(hello);
+RTCDTMFToneChangeEvent;
+globalThis;
+JSON;
+length;
+console.log(chfgbnvvbnvbnvbnvbn);
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+for (const iterator of object) {
+    
+}
+
+array.forEach(element => {
+    
+});
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
